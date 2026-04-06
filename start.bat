e2af16a5680a36e44693d283f55eaaa4e1624266
jack A siegel <jack@siegel.io>
2026-04-06 17:14:49 +0900

    Clean repo: only public/, start.sh, README, .gitignore

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0\public"
-echo.
-echo   Bananagrams running at http://localhost:3001
-echo   Open that URL in your browser to play!
-echo   Press Ctrl+C to stop
-echo.
-python -m http.server 3001
